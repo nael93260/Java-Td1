800d883bec5298f6af20c7cd236ecf4b6244ef2d
nael93260 <nael.talbi@gmail.com>
2021-11-23 11:15:54 +0100

    2nd commit

---
+++ Point.java
@@ -0,0 +1,11 @@
+package fr.dauphine.javaavance.td1;
+
+public class Point {
+	private int x;
+	private int y;
+
+	public static void main(String[] args) {
+		Point p = new Point();
+		System.out.println(p.x + " " + p.y);
+	}
+}
---
+++ Test.java
@@ -0,0 +1,13 @@
+package fr.dauphine.javaavance.td1;
+
+public class Test {
+	int foo;
+
+	public void setFoo(int foo) {
+		this.foo = foo;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
